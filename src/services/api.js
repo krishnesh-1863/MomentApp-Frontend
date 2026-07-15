88199250746884b8f6495d5fcb705c4d33a77404
krishnesh-1863 <2024uee1863@mnit.ac.in>
2026-07-15 23:51:49 +0530

    Add environment variables

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://moment-maker-backend.onrender.com", // apna backend port
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true
 });
 
